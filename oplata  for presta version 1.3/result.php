c6835970bb784deb2509ee4220693122801dedb1
olegliubiviy <oleg.liubiviy@gmail.com>
2014-09-12 09:43:36 +0300

    Updated displaying errors and success messages
    Updated displaying errors and success messages

--- oplata  for presta version 1.3/result.php
+++ oplata  for presta version 1.3/result.php
@@ -2,37 +2,49 @@
 
 require_once(dirname(__FILE__).'/oplata.php');
 require_once(dirname(__FILE__).'/oplata.cls.php');
+require_once _PS_CLASS_DIR_ . 'Mail.php';
 
 class OplataResultModuleFrontController extends Oplata
 {
-    private function showError($error)
+    /**
+     * @var Order
+     */
+    protected $_order;
+    /**
+     * @var Customer
+     */
+    protected $_customer;
+
+    private function showError($subject)
     {
-        global $smarty;
+        $id_lang = intval($this->_order->id_lang);
+        $to = $this->_customer->email;
+        $toName = $this->_customer->firstname . ' ' . $this->_customer->lastname;
 
-        $smarty->assign('message', $error);
+        $message = new Swift_Message('['.Configuration::get('PS_SHOP_NAME').'] '.$subject);
 
-        // Display all and exit
-        include(_PS_ROOT_DIR_.'/header.php');
-        echo $this->display(__FILE__, 'error.tpl');
-        include(_PS_ROOT_DIR_.'/footer.php');
-        die ;
-    }
+        $data = array(
+            '{lastname}' => $this->_customer->lastname,
+            '{firstname}' => $this->_customer->firstname,
+            '{shop_url}' => 'http://'.Tools::getHttpHost(false, true).__PS_BASE_URI__,
+            '{shop_name}' => Configuration::get('PS_SHOP_NAME'),
+            '{shop_logo}' => (file_exists(_PS_IMG_DIR_.'logo.jpg')) ? $message->attach(new Swift_Message_Image(new Swift_File(_PS_IMG_DIR_.'logo.jpg'))) : '',
+            '{id_order}' => intval($this->_order->id)
+        );
 
-    private function showSuccess($message)
-    {
-        global $smarty;
+        Mail::Send($id_lang, 'payment_error', $subject, $data, $to, $toName);
 
-        $smarty->assign('message', $message);
-
-        // Display all and exit
-        include(_PS_ROOT_DIR_.'/header.php');
-        echo $this->display(__FILE__, 'success.tpl');
-        include(_PS_ROOT_DIR_.'/footer.php');
-        die ;
+        // redirect to error with message
+        Tools::redirectLink(__PS_BASE_URI__.'modules/oplata/result-error.php?message=' . urlencode($subject));
     }
 
     public function postProcess()
     {
+        list($orderId,) = explode(OplataCls::ORDER_SEPARATOR, $_POST['order_id']);
+        $this->_order = new Order(intval($orderId));
+
+        $this->_customer = new Customer($this->_order->id_customer);
+
         if ($_POST['order_status'] == OplataCls::ORDER_DECLINED) {
             $this->showError(Tools::displayError('Order declined'));
         }
@@ -47,11 +59,7 @@ class OplataResultModuleFrontController extends Oplata
             $this->showError(Tools::displayError($isPaymentValid));
         }
 
-        list($orderId,) = explode(OplataCls::ORDER_SEPARATOR, $_POST['order_id']);
-        $order = new Order(intval($orderId));
-
-        $customer = new Customer($order->id_customer);
-        if (!Validate::isLoadedObject($customer)) {
+        if (!Validate::isLoadedObject($this->_customer)) {
             Tools::redirectLink(__PS_BASE_URI__.'order.php?step=1');
         }
 
@@ -63,7 +71,8 @@ class OplataResultModuleFrontController extends Oplata
         $history->changeIdOrderState(intval($id_order_state), intval($orderId));
         $history->addWithemail(true, "");
 
-        $this->showSuccess('Payment was successful');
+        // redirect to success
+        Tools::redirectLink(__PS_BASE_URI__.'modules/oplata/result-success.php');
     }
 }
 
