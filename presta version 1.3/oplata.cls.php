7e40fd833b142c6f2c09db8c7e43d2c3de711a9c
masiandr <max.dnu@gmail.com>
2016-01-20 16:17:45 +0200

    fix naming

---
+++ presta version 1.3/oplata.cls.php
@@ -0,0 +1,91 @@
+<?php
+
+class OplataCls
+{
+    const ORDER_APPROVED = 'approved';
+    const ORDER_DECLINED = 'declined';
+
+    const ORDER_SEPARATOR = '#';
+
+    const SIGNATURE_SEPARATOR = '|';
+
+    const URL = "https://api.fondy.eu/api/checkout/redirect/";
+
+    protected static $responseFields = array('rrn',
+        'masked_card',
+        'sender_cell_phone',
+        'response_status',
+        'currency',
+        'fee',
+        'reversal_amount',
+        'settlement_amount',
+        'actual_amount',
+        'order_status',
+        'response_description',
+        'order_time',
+        'actual_currency',
+        'order_id',
+        'tran_type',
+        'eci',
+        'settlement_date',
+        'payment_system',
+        'approval_code',
+        'merchant_id',
+        'settlement_currency',
+        'payment_id',
+        'sender_account',
+        'card_bin',
+        'response_code',
+        'card_type',
+        'amount',
+        'sender_email');
+
+    public static function getSignature($data, $password, $encoded = true)
+    {
+        $data = array_filter($data, function($var) {
+            return $var !== '' && $var !== null;
+        });
+        ksort($data);
+
+        $str = $password;
+        foreach ($data as $k => $v) {
+            if($k != 'response_signature_string') {
+                $str .= self::SIGNATURE_SEPARATOR . $v;
+            }
+        }
+
+        if ($encoded) {
+            return sha1($str);
+        } else {
+            return $str;
+        }
+    }
+
+    public static function isPaymentValid($oplataSettings, $response)
+    {
+        if ($oplataSettings['merchant_id'] != $response['merchant_id']) {
+            return 'An error has occurred during payment. Merchant data is incorrect.';
+        }
+
+        if ($response['order_status'] == self::ORDER_DECLINED) {
+            return 'An error has occurred during payment. Order is declined.';
+        }
+
+        $originalResponse = $response;
+        foreach ($response as $k => $v) {
+            if (!in_array($k, self::$responseFields)) {
+                unset($response[$k]);
+            }
+        }
+
+        if (self::getSignature($response, $oplataSettings['secret_key']) != $originalResponse['signature']) {
+            return 'An error has occurred during payment. Signature is not valid.';
+        }
+
+//        if ($response['order_status'] != self::ORDER_APPROVED) {
+//            return false;
+//        }
+
+        return true;
+    }
+}
---
+++ presta version 1.3/result.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once(dirname(__FILE__).'/oplata.php');
+require_once(dirname(__FILE__).'/oplata.cls.php');
+require_once _PS_CLASS_DIR_ . 'Mail.php';
+
+class OplataResultModuleFrontController extends Oplata
+{
+    /**
+     * @var Order
+     */
+    protected $_order;
+    /**
+     * @var Customer
+     */
+    protected $_customer;
+
+    private function showError($subject)
+    {
+        $id_lang = intval($this->_order->id_lang);
+        $to = $this->_customer->email;
+        $toName = $this->_customer->firstname . ' ' . $this->_customer->lastname;
+
+        $message = new Swift_Message('['.Configuration::get('PS_SHOP_NAME').'] '.$subject);
+
+        $data = array(
+            '{lastname}' => $this->_customer->lastname,
+            '{firstname}' => $this->_customer->firstname,
+            '{shop_url}' => 'http://'.Tools::getHttpHost(false, true).__PS_BASE_URI__,
+            '{shop_name}' => Configuration::get('PS_SHOP_NAME'),
+            '{shop_logo}' => (file_exists(_PS_IMG_DIR_.'logo.jpg')) ? $message->attach(new Swift_Message_Image(new Swift_File(_PS_IMG_DIR_.'logo.jpg'))) : '',
+            '{id_order}' => intval($this->_order->id)
+        );
+
+        Mail::Send($id_lang, 'payment_error', $subject, $data, $to, $toName);
+
+        // redirect to error with message
+        Tools::redirectLink(__PS_BASE_URI__.'modules/oplata/result-error.php?message=' . urlencode($subject));
+    }
+
+    public function postProcess()
+    {
+        list($orderId,) = explode(OplataCls::ORDER_SEPARATOR, $_POST['order_id']);
+        $this->_order = new Order(intval($orderId));
+
+        $this->_customer = new Customer($this->_order->id_customer);
+
+        if ($_POST['order_status'] == OplataCls::ORDER_DECLINED) {
+            $this->showError(Tools::displayError('Order declined'));
+        }
+
+        $settings = array(
+            'merchant_id' => $this->getOption('merchant'),
+            'secret_key' => $this->getOption('secret_key')
+        );
+
+        $isPaymentValid = OplataCls::isPaymentValid($settings, $_POST);
+        if ($isPaymentValid !== true) {
+            $this->showError(Tools::displayError($isPaymentValid));
+        }
+
+        if (!Validate::isLoadedObject($this->_customer)) {
+            Tools::redirectLink(__PS_BASE_URI__.'order.php?step=1');
+        }
+
+        $history = new OrderHistory();
+        $history->id_order = $orderId;
+
+        $id_order_state = _PS_OS_PAYMENT_;
+
+        $history->changeIdOrderState(intval($id_order_state), intval($orderId));
+        $history->addWithemail(true, "");
+
+        // redirect to success
+        Tools::redirectLink(__PS_BASE_URI__.'modules/oplata/result-success.php');
+    }
+}
+
+$result = new OplataResultModuleFrontController();
+$result->postProcess();
